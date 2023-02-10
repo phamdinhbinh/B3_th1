aacee526aada88c12b8eaf69cce3e6fcc04c8377
phamdinhbinh <phamdinhbinhonly1@gmail.com>
2023-02-10 18:28:24 +0700

    th1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import photo1 from './photo1.jpg';
+import { base64Photo } from './Constant';
+// import Logo from './logo.svg';
+// import DeleteIcon from './DeleteIcon';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div>Đây là hướng dẫn dùng ảnh trong ReactJs</div>
+      <img src={photo1} width="200" height="100" className='img' />
+      <div className='require-title'>Đây là ảnh dùng require</div>
+      <img src={require('./photo1.jpg')} className='img2' />
+      <div className='require-title'>Đây là ảnh dùng link online</div>
+      <img src='https://i.imgur.com/0BJobQo.jpg' className='img2' />
+      <div className='require-title'>Đây là ảnh dùng base64</div>
+      <img src={base64Photo} className='img3' />
+      <div className='require-title'>Đây là ảnh dùng svg</div>
+      {/* <img src={Logo} className='img3' />
+      <DeleteIcon /> */}
     </div>
   );
 }
-
-export default App;
